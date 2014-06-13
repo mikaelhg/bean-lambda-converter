d8e598177c9522767b50759ba5ae05c8c98844e4
Mikael Gueck <gumi@iki.fi>
2014-06-13 19:58:07 +0300

    Test legibility.

--- src/test/java/io/mikael/convert/basic/BasicTests.java
+++ src/test/java/io/mikael/convert/basic/BasicTests.java
@@ -9,84 +9,84 @@ import static org.mockito.Mockito.*;
 
 public class BasicTests {
 
-    public static interface Foo {
-        public String getFirstName();
+    public static interface TestSource {
+        public String getInput();
     }
 
-    public static interface Bar {
-        public void setName(final String b);
+    public static interface TestTarget {
+        public void setOutput(final String output);
     }
 
-    private Foo foo;
-    private Bar bar;
+    private TestSource testSource;
+    private TestTarget testTarget;
 
     @Before
     public void before() {
-        foo = mock(Foo.class);
-        bar = mock(Bar.class);
-        when(foo.getFirstName()).thenReturn("A");
+        testSource = mock(TestSource.class);
+        testTarget = mock(TestTarget.class);
+        when(testSource.getInput()).thenReturn("A");
     }
 
     @Test
-    public void basicTest() {
-        BeanConverter.<Foo, Bar>fromTo()
-                .field(Foo::getFirstName, Bar::setName)
-                .convert(foo, bar);
-        verify(bar).setName("A");
+    public void fieldConversionWithGenerics() {
+        BeanConverter.<TestSource, TestTarget>fromTo()
+                .field(TestSource::getInput, TestTarget::setOutput)
+                .convert(testSource, testTarget);
+        verify(testTarget).setOutput("A");
     }
 
     @Test
-    public void basicTest2() {
-        BeanConverter.fromTo(Foo.class, Bar.class)
-                .field(Foo::getFirstName, Bar::setName)
-                .convert(foo, bar);
-        verify(bar).setName("A");
+    public void fieldConversionWithClassParameters() {
+        BeanConverter.fromTo(TestSource.class, TestTarget.class)
+                .field(TestSource::getInput, TestTarget::setOutput)
+                .convert(testSource, testTarget);
+        verify(testTarget).setOutput("A");
     }
 
     @Test
-    public void basicTest3() {
-        BeanConverter.fromTo(Foo.class, Bar.class)
+    public void fieldGroupWithoutPrecondition() {
+        BeanConverter.fromTo(TestSource.class, TestTarget.class)
                 .fieldGroup()
-                    .field(Foo::getFirstName, Bar::setName)
+                    .field(TestSource::getInput, TestTarget::setOutput)
                 .end()
-                .convert(foo, bar);
-        verify(bar).setName("A");
+                .convert(testSource, testTarget);
+        verify(testTarget).setOutput("A");
     }
 
     @Test
     public void basicFieldGroupNotCalled() {
-        BeanConverter.fromTo(Foo.class, Bar.class)
-                .fieldGroup(f -> f.getFirstName().equals("I AM A TEAPOT"))
-                    .field(Foo::getFirstName, Bar::setName)
+        BeanConverter.fromTo(TestSource.class, TestTarget.class)
+                .fieldGroup(f -> f.getInput().equals("I AM A TEAPOT"))
+                    .field(TestSource::getInput, TestTarget::setOutput)
                 .end()
-                .convert(foo, bar);
-        verifyZeroInteractions(bar);
+                .convert(testSource, testTarget);
+        verifyZeroInteractions(testTarget);
     }
 
     @Test
     public void basicFieldGroupCalled() {
-        BeanConverter.fromTo(Foo.class, Bar.class)
-                .fieldGroup(f -> f.getFirstName().equals("A"))
-                    .field(Foo::getFirstName, Bar::setName)
+        BeanConverter.fromTo(TestSource.class, TestTarget.class)
+                .fieldGroup(f -> f.getInput().equals("A"))
+                    .field(TestSource::getInput, TestTarget::setOutput)
                 .end()
-                .convert(foo, bar);
-        verify(bar).setName("A");
+                .convert(testSource, testTarget);
+        verify(testTarget).setOutput("A");
     }
 
     @Test
-    public void testSources() {
-        final BeanConverter<Foo, Bar> bc = BeanConverter.fromTo(Foo.class, Bar.class);
+    public void converterSourceSignatures() {
+        final BeanConverter<TestSource, TestTarget> bc = BeanConverter.fromTo(TestSource.class, TestTarget.class);
 
-        final Bar b1 = bc.convert(() -> mock(Foo.class), () -> mock(Bar.class));
+        final TestTarget b1 = bc.convert(() -> mock(TestSource.class), () -> mock(TestTarget.class));
         assertNotNull(b1);
 
-        final Bar b2 = bc.convert(mock(Foo.class), () -> mock(Bar.class));
+        final TestTarget b2 = bc.convert(mock(TestSource.class), () -> mock(TestTarget.class));
         assertNotNull(b2);
 
-        final Bar b3 = bc.convert(() -> mock(Foo.class), mock(Bar.class));
+        final TestTarget b3 = bc.convert(() -> mock(TestSource.class), mock(TestTarget.class));
         assertNotNull(b3);
 
-        verifyZeroInteractions(foo, bar);
+        verifyZeroInteractions(testSource, testTarget);
     }
 
 }

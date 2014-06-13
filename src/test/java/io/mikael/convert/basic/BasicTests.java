f70d3c79e0bb53eecd2dfb82596a8a6ca3a4f0c9
Mikael Gueck <gumi@iki.fi>
2014-06-13 19:54:34 +0300

    Add Mockito tests for conversion calls.

--- src/test/java/io/mikael/convert/basic/BasicTests.java
+++ src/test/java/io/mikael/convert/basic/BasicTests.java
@@ -1,69 +1,92 @@
 package io.mikael.convert.basic;
 
 import io.mikael.convert.bean.BeanConverter;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.*;
 
 public class BasicTests {
 
-    public static class Foo {
-        public String getFirstName() { return "A"; }
-        public void invalid(final String in) { System.err.println("INVALID"); }
+    public static interface Foo {
+        public String getFirstName();
     }
 
-    public static class Foo2 extends Foo {
-        @Override
-        public String getFirstName() { return "B"; }
+    public static interface Bar {
+        public void setName(final String b);
     }
 
-    public static class Bar {
-        public void setName(final String b) { System.out.println("b: " + b); }
-        public String invalid() { System.err.println("INVALID"); return "INVALID"; }
+    private Foo foo;
+    private Bar bar;
+
+    @Before
+    public void before() {
+        foo = mock(Foo.class);
+        bar = mock(Bar.class);
+        when(foo.getFirstName()).thenReturn("A");
     }
 
     @Test
     public void basicTest() {
-        final BeanConverter<Foo, Bar> c1 = BeanConverter.<Foo, Bar>fromTo()
-                .field(Foo::getFirstName, Bar::setName);
+        BeanConverter.<Foo, Bar>fromTo()
+                .field(Foo::getFirstName, Bar::setName)
+                .convert(foo, bar);
+        verify(bar).setName("A");
     }
 
     @Test
     public void basicTest2() {
-        final BeanConverter<Foo, Bar> c2 = BeanConverter.fromTo(Foo.class, Bar.class)
-                .field(Foo::getFirstName, Bar::setName);
+        BeanConverter.fromTo(Foo.class, Bar.class)
+                .field(Foo::getFirstName, Bar::setName)
+                .convert(foo, bar);
+        verify(bar).setName("A");
     }
 
     @Test
     public void basicTest3() {
-        final BeanConverter<Foo, Bar> c3 = BeanConverter.fromTo(Foo.class, Bar.class)
+        BeanConverter.fromTo(Foo.class, Bar.class)
                 .fieldGroup()
                     .field(Foo::getFirstName, Bar::setName)
-                .end();
+                .end()
+                .convert(foo, bar);
+        verify(bar).setName("A");
     }
 
     @Test
-    public void basicTest4() {
-        final BeanConverter<Foo, Bar> c4 = BeanConverter.fromTo(Foo.class, Bar.class)
-                .fieldGroup(f -> f.getFirstName().equals("I AM A TEAPOT"))
-                    .field(Foo::getFirstName, Bar::setName)
-                .end();
-
-        final Foo foo = new Foo() {
-            @Override public String getFirstName() { return "C"; }
-        };
-
-        final Foo2 foo2 = new Foo2();
-
-        final Bar bar = BeanConverter.fromTo(Foo.class, Bar.class)
+    public void basicFieldGroupNotCalled() {
+        BeanConverter.fromTo(Foo.class, Bar.class)
                 .fieldGroup(f -> f.getFirstName().equals("I AM A TEAPOT"))
                     .field(Foo::getFirstName, Bar::setName)
                 .end()
-                .convert(foo, new Bar());
+                .convert(foo, bar);
+        verifyZeroInteractions(bar);
+    }
 
-        final Bar bar2 = BeanConverter.fromTo(Foo.class, Bar.class)
-                .fieldGroup(f -> f.getFirstName().equals("I AM A TEAPOT"))
+    @Test
+    public void basicFieldGroupCalled() {
+        BeanConverter.fromTo(Foo.class, Bar.class)
+                .fieldGroup(f -> f.getFirstName().equals("A"))
                     .field(Foo::getFirstName, Bar::setName)
                 .end()
-                .convert(foo2, Bar::new);
+                .convert(foo, bar);
+        verify(bar).setName("A");
+    }
+
+    @Test
+    public void testSources() {
+        final BeanConverter<Foo, Bar> bc = BeanConverter.fromTo(Foo.class, Bar.class);
+
+        final Bar b1 = bc.convert(() -> mock(Foo.class), () -> mock(Bar.class));
+        assertNotNull(b1);
+
+        final Bar b2 = bc.convert(mock(Foo.class), () -> mock(Bar.class));
+        assertNotNull(b2);
+
+        final Bar b3 = bc.convert(() -> mock(Foo.class), mock(Bar.class));
+        assertNotNull(b3);
+
+        verifyZeroInteractions(foo, bar);
     }
 
 }
